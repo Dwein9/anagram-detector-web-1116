77511a156b537bcdf037947648fcd97a0fa7678b
Dwein9 <Dwein9@gmail.com>
2016-11-30 21:38:45 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :main
+
+  def initialize(word)
+    @main = word
+  end
+
+  def ordered(word)
+    word.split("").sort
+  end
+
+  def match(word_array)
+    word_array.select do |word|
+      ordered(main) == ordered(word)
+    end
+  end
+
+end
